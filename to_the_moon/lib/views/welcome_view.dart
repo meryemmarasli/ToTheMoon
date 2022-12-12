b56b400d5ddbb5712930aeec9397b9aef62e2c6d
chh147 <chh147@pitt.edu>
2022-12-12 16:36:59 -0500

    move confetti to neater space. move welcome title slightly up

--- to_the_moon/lib/views/welcome_view.dart
+++ to_the_moon/lib/views/welcome_view.dart
@@ -9,16 +9,14 @@ import 'package:to_the_moon/views/navigationBar.dart';
 
 import '../models/User.dart';
 
-
-
-class WelcomeView extends StatefulWidget{
+class WelcomeView extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return new WelcomeViewState();
   }
 }
 
-class WelcomeViewState extends State<WelcomeView>{
+class WelcomeViewState extends State<WelcomeView> {
   bool accept = false;
   @override
   Widget build(BuildContext context) {
@@ -26,63 +24,65 @@ class WelcomeViewState extends State<WelcomeView>{
     getAccept(userViewModel.getUser());
 
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 6, 27, 44),
-      body:
-        Stack(
-        children: <Widget>[
-           Align(
-          alignment: Alignment(0.0, -0.39),
-          child: Container(
-            child: Text("to the moon", style:TextStyle(color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic)), 
-          ),
-        ),
+        backgroundColor: Color.fromARGB(255, 6, 27, 44),
+        body: Stack(children: <Widget>[
           Align(
-              alignment: Alignment(0.0, 0),
-
-              child: Container(
-                child: new Image.asset('assets/images/rocketGif.gif'),
-                alignment: Alignment.center,
-                width: 350,
-                height: 250,
-              ),
+            alignment: Alignment(0.0, -0.45),
+            child: Container(
+              child: Text("to the moon",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic)),
+            ),
           ),
-         
-        Align(
-              child: Container(
-                  alignment: Alignment(0.0, 0.5),
-                 child: SizedBox(
-                    height: 50,
-                    width:100,
-                      child:ElevatedButton(
-                          child: Text('Start!', style:TextStyle(color: Colors.white,)),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color.fromARGB(255, 220, 35, 22),
-                            shape: StadiumBorder() ,
-                            )
-                          ,
-                          onPressed: () {
-                            if(accept){
-                                Navigator.push(context, MaterialPageRoute(builder: (context) => BottomBar()));
-                            }else{
-                              Navigator.push(context, MaterialPageRoute(builder: (context) => AgreementView()));
-
-                            }
-                          }
-                      )
-                  ),
-              ),
-        ),
-         
-    ])
-    );
+          Align(
+            alignment: Alignment(0.0, 0),
+            child: Container(
+              child: new Image.asset('assets/images/rocketGif.gif'),
+              alignment: Alignment.center,
+              width: 350,
+              height: 250,
+            ),
+          ),
+          Align(
+            child: Container(
+              alignment: Alignment(0.0, 0.5),
+              child: SizedBox(
+                  height: 50,
+                  width: 100,
+                  child: ElevatedButton(
+                      child: Text('Start!',
+                          style: TextStyle(
+                            color: Colors.white,
+                          )),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 220, 35, 22),
+                        shape: StadiumBorder(),
+                      ),
+                      onPressed: () {
+                        if (accept) {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => BottomBar()));
+                        } else {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => AgreementView()));
+                        }
+                      })),
+            ),
+          ),
+        ]));
   }
 
-  getAccept(Future<UserModel> f) async{
-     UserModel user = await f;
-      setState(() {
-        accept = user.acceptAgreement;
-      });
+  getAccept(Future<UserModel> f) async {
+    UserModel user = await f;
+    setState(() {
+      accept = user.acceptAgreement;
+    });
   }
-
 }
-
